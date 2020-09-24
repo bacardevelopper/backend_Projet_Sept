6e92a1b4fd4c60d9d867e8b4990c15ce850382bb
bacardevelopper <bacar.darwin.github@hotmail.com>
2020-09-24 13:03:43 +0200

    first inrt into

--- backend_p_7/controllers/controllersID.js
+++ backend_p_7/controllers/controllersID.js
@@ -1,44 +1,45 @@
 /* modules used */
-const mysql = require('mysql');
+const mysql = require("mysql");
 /* modules used */
-const connection = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
-  password : '',
-  database : 'nodejs'
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "nodejs",
 });
 
 /* FUNCTION CREATE USER (SIGNUP) */
 /* à la reussite finale , l'utilisateur doit etre ajouter et server renvoit l'id */
 exports.createUser = (req, res, next) => {
-  if(req){
-    if(req.body.email !== "" && req.body.mdp !== ""){
-      res.status(200).json({ message : 'ok données bien reçu'});
+  if (req) {
+    if (req.body.email !== "" && req.body.mdp !== "") {
+      res.status(200).json({ message: "ok données bien reçu" });
+      let email = req.body.email;
+      let password = req.body.mdp;
+
+      //sql requete
+      const sql = `INSERT INTO members (adressemail, password) VALUES('${email}', '${password}')`;
       console.log(req.body);
 
-      connection.connect((err) => {
-
-        if(err){
-          console.log('not connected in database');
-
-        }else{
-          console.log('connected in database');
-          connection.query('SELECT * FROM members', (error, results) => {
-            if(error){
-              console.log(error.message);
-            }else{
-              /* [ RowDataPacket { id: 8, adressemail: 'le email', password: '' } ] */
-              console.log(results);
+      /* CONNECTION MYSQL DB */
+      db.connect((err) => {
+        if (err) {
+          console.log("not connected in database");
+        } else {
+          console.log("connected in database");
+          db.query(sql,(error, results) => {
+              if (error) {
+                console.log(error.message);
+              } else {
+                /* [ RowDataPacket { id: 8, adressemail: 'le email', password: '' } ] */
+                console.log(results);
+              }
             }
-          });
+          );
         }
       });
-
-
     }
-
-  }else{
-    return res.status(400).json({messahge : 'erreur'});
+  } else {
+    return res.status(400).json({ messahge: "erreur" });
   }
-
-}
+};
